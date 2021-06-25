cc5b0373bb2c53b037014629ae83165e4ce14d1f
sanja-k-hristov <sanja.k.hristov@gmail.com>
2021-06-26 00:09:46 +0200

    homework 4 added

---
+++ Session 4 Homework - 02 Scalar functions.sql
@@ -0,0 +1,31 @@
+/*
+Declare scalar function (fn_FormatStudentName) for retrieving the Student description for specific StudentId in the following format:
+	- StudentCardNumber without “sc-”
+	- “ – “
+	- First character of student FirstName
+	- “.”
+	- Student LastName
+	
+	example:
+	sc-77712 => 77712-P.Manaskov
+
+*/
+
+ALTER FUNCTION dbo.fn_FormatStudentName (@StudentId int)
+RETURNS Nvarchar(100)
+AS 
+BEGIN
+
+DECLARE @Output Nvarchar(100)
+
+select @Output = Replace(StudentCardNumber,'sc-','') + ' - ' + Left(FirstName,1) + '.' + LastName
+from dbo.Student
+where id = @StudentId
+
+RETURN @Output
+
+END
+GO
+
+select *, dbo.fn_FormatStudentName(id) as FunctionOutput
+from dbo.Student
